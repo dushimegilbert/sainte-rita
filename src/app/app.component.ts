0bcb2a652338c5fb9aa17da23f9bd88daaa457ac
Dushime Gilbert <dushimegilbert124@.com>
2025-03-08 18:03:22 +0200

    finetune page not found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
